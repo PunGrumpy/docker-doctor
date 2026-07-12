0660aa145e8244d9f6dcf4de45f7ffdeb7d2f13a
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2026-07-12 23:18:12 +0700

    refactor: optimize SVG path data in icon components

--- apps/web/components/icons/claude-code.tsx
+++ apps/web/components/icons/claude-code.tsx
@@ -2,21 +2,17 @@ import type { SVGProps } from "react";
 
 export const ClaudeCode = ({ ...props }: SVGProps<SVGSVGElement>) => (
   <svg
-    viewBox="0 0 217 144"
-    fill="none"
     xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 217 144"
     {...props}
   >
     <path
-      d="M216.06 57.69H188.18V0H27.88V57.69H0V86.85H27.44V114.73H41.28V143.89H55.57V114.73H68.52V143.45H82.36V115.17H133.42V143.89H147.71V115.17H160.66V143.45H174.06V115.17H187.91V86.85H216.02V57.69H216.06Z"
-      fill="#F76038"
+      fill="#f76038"
+      d="M216.06 57.69h-27.88V0H27.88v57.69H0v29.16h27.44v27.88h13.84v29.16h14.29v-29.16h12.95v28.72h13.84v-28.28h51.06v28.72h14.29v-28.72h12.95v28.28h13.4v-28.28h13.85V86.85h28.11V57.69z"
     />
     <path
-      d="M55.63 29.61H68.58V57.69H55.63V29.61Z"
-      className="fill-white dark:fill-[#151515]"
-    />
-    <path
-      d="M147.76 29.83H160.71V57.69H147.76V29.83Z"
+      d="M55.63 29.61h12.95v28.08H55.63zm92.13.22h12.95v27.86h-12.95z"
       className="fill-white dark:fill-[#151515]"
     />
   </svg>
